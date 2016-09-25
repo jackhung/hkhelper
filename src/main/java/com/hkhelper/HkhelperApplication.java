9525e009d19c38e9b97b4990b258d18137819897
JackHung <hungjk@gmail.com>
2016-09-25 15:55:01 +0800

    initial commit

---
+++ src/main/java/com/hkhelper/HkhelperApplication.java
@@ -0,0 +1,12 @@
+package com.hkhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkhelperApplication.class, args);
+	}
+}
